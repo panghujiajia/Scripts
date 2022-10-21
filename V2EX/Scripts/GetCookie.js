eda98b64f20f7accdec7de1453e3059230e22312
panghujiajia <pumbaa1024@gmail.com>
2022-10-21 20:57:07 +0800

    bug fix

--- V2EX/Scripts/GetCookie.js
+++ V2EX/Scripts/GetCookie.js
@@ -3,12 +3,10 @@
 try {
 	const { headers } = $request;
 	const { Cookie } = headers;
-	if (!Cookie || Cookie.indexOf('A2=' < 0)) {
+	if (!Cookie || Cookie.indexOf('A2=') < 0) {
 		console.log('\n================================================\n');
 		console.log('获取Cookie失败：');
 		console.log(JSON.stringify(headers));
-		console.log('\n================================================\n');
-		console.log(Cookie);
 		console.log('\n================================================\n');
 
 		$notify('V2EX', `Cookie获取失败！`);
@@ -17,8 +15,7 @@ try {
 		return;
 	}
 	$prefs.setValueForKey(Cookie, 'V2EX_COOKIE');
-	$notify('V2EX', `Cookie写入成功！`
-		, `Cookie：${Cookie}`);
+	$notify('V2EX', `Cookie写入成功！`, `Cookie：${Cookie}`);
 	$done();
 } catch (err) {
 	console.log('\n========================error========================\n');
