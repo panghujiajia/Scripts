ea17814b7b5d39ee3633e4bd1357a5c71d818d1e
panghujiajia <pumbaa1024@gmail.com>
2022-10-22 11:14:18 +0800

    fix

--- V2EX/Scripts/SignIn.js
+++ V2EX/Scripts/SignIn.js
@@ -20,8 +20,10 @@ const headers = {
     Connection: `keep-alive`,
     Accept: `text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8`,
     Host: `www.v2ex.com`,
-    'User-Agent': `Mozilla/5.0 (iPhone; CPU iPhone OS 16_0_3 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/16.0 Mobile/15E148 Safari/604.1`,
-    'Accept-Language': `zh-CN,zh-Hans;q=0.9`
+    'Accept-Language': `zh-CN,zh-Hans;q=0.9`,
+    Referer: 'https://www.v2ex.com/mission/daily',
+    'User-Agent':
+        'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.93 Safari/537.36'
 };
 
 getSigninInfo();
